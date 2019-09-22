77aee74dd4539eab9b21c9c69fdc77ca2d6386cf
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-22 16:03:00 -0500

    finished first practice on codingbat

--- src/Lemon.java
+++ src/Lemon.java
@@ -1,6 +1,5 @@
 import java.text.DateFormatSymbols;
 import java.util.Scanner;
-
 public class Lemon {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -17,39 +16,35 @@ public class Lemon {
 
         //-------------------- BONUS 2----------------
 
-        System.out.println("Enter a sentence: ");
-        String userInput = input.nextLine();
-        int vowels = 0, consonants = 0;
-
-        userInput = userInput.toLowerCase();
-        for(int i = 0; i < userInput.length(); ++i)
-        {
-            char ch = userInput.charAt(i);
-            if(ch == 'a' || ch == 'e' || ch == 'i'
-                    || ch == 'o' || ch == 'u') {
-                ++vowels;
-            }
-            else if((ch >= 'a'&& ch <= 'z')) {
-                ++consonants;
-            }
-        }
-        System.out.println("Vowels: " + vowels);
-        System.out.println("Consonants: " + consonants);
-
-
-
-
-
-//        Create an application that allows the user to enter a sentence
-//        and tells them how many vowels and consonants were used.
+//        System.out.println("Enter a sentence: ");
+//        String userInput = input.nextLine();
+//        int vowels = 0, consonants = 0;
 //
-//        Example:
-//
-//        input - "The hill are alive."
-//        output - 5 vowels and 10 consonants
-        //-------------------- BONUS 3----------------
+//        userInput = userInput.toLowerCase();
+//        for(int i = 0; i < userInput.length(); ++i)
+//        {
+//            char ch = userInput.charAt(i);
+//            if(ch == 'a' || ch == 'e' || ch == 'i'
+//                    || ch == 'o' || ch == 'u') {
+//                ++vowels;
+//            }
+//            else if((ch >= 'a'&& ch <= 'z')) {
+//                ++consonants;
+//            }
+//        }
+//        System.out.println("Vowels: " + vowels);
+//        System.out.println("Consonants: " + consonants);
 
+        // Method Calls from bonus practice on methods
+//        System.out.println(sleepIn(false,false)); // true
+//        System.out.println(sleepIn(true,false)); // false
+//        System.out.println(sleepIn(false,true)); // true
+    }
 
+    //----- Practice Problems https://codingbat.com/java/Warmup-1 --------
 
+//    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
     }
 }
