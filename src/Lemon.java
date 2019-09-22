742ffdecf5257cdea8c7e459b9d562acee8ca805
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-22 16:28:15 -0500

    finished two more exercise

--- src/Lemon.java
+++ src/Lemon.java
@@ -36,9 +36,7 @@ public class Lemon {
 //        System.out.println("Consonants: " + consonants);
 
         // Method Calls from bonus practice on methods
-//        System.out.println(sleepIn(false,false)); // true
-//        System.out.println(sleepIn(true,false)); // false
-//        System.out.println(sleepIn(false,true)); // true
+
     }
 
     //----- Practice Problems https://codingbat.com/java/Warmup-1 --------
@@ -47,4 +45,21 @@ public class Lemon {
     public static boolean sleepIn(boolean weekday, boolean vacation) {
         return !weekday || vacation;
     }
+
+//    We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return aSmile == bSmile || !aSmile == !bSmile;
+    }
+
+//    Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+    public static int sumDouble(int a, int b) {
+        int sum = 0;
+        if(a == b) {
+            sum = (a + b) * 2;
+        }else {
+            sum = a + b;
+        }
+        return sum;
+    }
+
 }
